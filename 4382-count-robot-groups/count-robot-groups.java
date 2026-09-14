3112df95705bb5c93b87e7ee3654c6e7b404fbdb
Deepti Sharma <deeptisharma3410@gmail.com>
2026-09-14 14:45:14 +0530

    Time: 2 ms (100.00%) | Memory: 100.1 MB (48.85%) - LeetSync

--- 4382-count-robot-groups/count-robot-groups.java
+++ 4382-count-robot-groups/count-robot-groups.java
@@ -1,39 +1,21 @@
 class Solution {
     public int countGroups(int[] position, int[] speed, int distance) {
         int n = position.length;
-        double[] mergeTime = new double[n];
-        Arrays.fill(mergeTime, -1);
-        Stack<Integer> st = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty()) {
-                int j = st.peek();
-                double t;
-                long gap = (long) position[j] - position[i];
-                if (gap <= distance) {
-                    t = 0.0;
-                } else {
-                    long relSpeed = (long) speed[i] - speed[j];
-                    if (relSpeed <= 0) {
-                        t = Double.POSITIVE_INFINITY;
-                    } else {
-                        t = (double) (gap - distance) / relSpeed;
-                    }
-                }
-                if (t == Double.POSITIVE_INFINITY) {
-                    st.pop();
-                } else if (mergeTime[j] < 0 || t <= mergeTime[j]) {
-                    mergeTime[i] = t;
-                    break;
-                } else {
-                    st.pop();
-                }
+        int groups = n;
+        int maxSpeed = speed[n - 1];
+
+        for (int i = n - 1; i > 0; --i) {
+            if (position[i] - position[i - 1] <= distance) {
+                groups--;
             }
-            st.push(i);
+            else if (speed[i - 1] > maxSpeed) {
+                groups--;
+            }
+            else {
+                maxSpeed = speed[i - 1];
+            }
         }
-        int groups = 0;
-        for (double t : mergeTime) {
-            if (t < 0) groups++;
-        }
+
         return groups;
     }
 }
